1af01a21c41b5a1488281d96828f01d798daed85
Blew_Dreamer <103670494+amshunaik@users.noreply.github.com>
2025-03-02 11:04:26 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -4,8 +4,17 @@ require('dotenv').config();
 const cors=require('cors');
 const ConnectDB = require('./DB.js');
 const app=express();
+
+
 app.use(express.json());
-app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://guess-game-backend-ug7p.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+//app.use(cors())
 
 
 const PORT=3005;
